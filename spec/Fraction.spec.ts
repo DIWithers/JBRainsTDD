ab78b44b7a3ba6ec8fd010dc4c5abd1543958c12
DIWithers <d.ivy.withers@gmail.com>
2017-06-20 09:16:29 -0400

    Shorten tests by inlining temporary variables

--- spec/Fraction.spec.ts
+++ spec/Fraction.spec.ts
@@ -1,23 +1,18 @@
 import {Fraction} from "../src/Fraction";
 describe("AddFractionsTest", () => {
     it("zero plus zero", () => {
-        let sum: Fraction = new Fraction(0).plus(new Fraction(0));
-        expect(sum.intValue()).toEqual(0);
+        expect(new Fraction(0).plus(new Fraction(0)).intValue()).toEqual(0);
     });
     it("non-zero plus zero", () => {
-        let sum: Fraction = new Fraction(3).plus(new Fraction(0));
-        expect(sum.intValue()).toEqual(3);
+        expect(new Fraction(3).plus(new Fraction(0)).intValue()).toEqual(3);
     });
     it("zero plus non-zero", () => {
-        let sum: Fraction = new Fraction(0).plus(new Fraction(5));
-        expect(sum.intValue()).toEqual(5);
+        expect(new Fraction(0).plus(new Fraction(5)).intValue()).toEqual(5);
     });
     it("non-negative, non-zero operands ", () => {
-        let sum: Fraction = new Fraction(3).plus(new Fraction(4));
-        expect(sum.intValue()).toEqual(7);
+        expect(new Fraction(3).plus(new Fraction(4)).intValue()).toEqual(7);
     });
     it("negative inputs and negative outputs", () => {
-        let sum: Fraction = new Fraction(-3).plus(new Fraction(1));
-        expect(sum.intValue()).toEqual(-2);
+        expect(new Fraction(-3).plus(new Fraction(1)).intValue()).toEqual(-2);
     });
 });
