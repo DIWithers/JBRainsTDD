ed353d212fb46d7bcbc33ed13f4a946283c5e73b
DIWithers <d.ivy.withers@gmail.com>
2017-06-21 11:58:37 -0400

    Denominator no longer faked

--- src/Fraction.ts
+++ src/Fraction.ts
@@ -1,11 +1,13 @@
 export class Fraction {
+    denominator: number = 1;
     integerValue: number;
     constructor(numerator: number, denominator?: number) {
         this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     plus(that: Fraction): Fraction {
-        return new Fraction(this.integerValue + that.integerValue);
+        return new Fraction(this.integerValue + that.integerValue, this.denominator);
     }
     intValue(): number {
        return this.integerValue;
@@ -14,7 +16,7 @@ export class Fraction {
         return 3;
     }
     getDenominator(): number {
-        return 5;
+        return this.denominator;
     }
 
 }
