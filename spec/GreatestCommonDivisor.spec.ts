0127c41a5777bc26413f35ac344437f3db715a6c
DIWithers <d.ivy.withers@gmail.com>
2017-06-22 12:56:13 -0400

    added convention that GCD is always positive

--- spec/GreatestCommonDivisor.spec.ts
+++ spec/GreatestCommonDivisor.spec.ts
@@ -8,18 +8,18 @@ describe("GreatestCommonDivisorTest", () => {
             b = a % t;
             a = t;
         }
-        return a;
+        return Math.abs(a);
     }
 
     it("reflexive examples", () => {
         expect(gcd(1, 1)).toEqual(1);
         expect(gcd(2, 2)).toEqual(2);
-        expect(gcd(-1, -1)).toEqual(-1);
+        expect(gcd(-1, -1)).toEqual(1);
     });
     it("relatively prime examples", () => {
         expect(gcd(2, 3)).toEqual(1);
         expect(gcd(4, 7)).toEqual(1);
-        expect(gcd(-2, -3)).toEqual(-1);
+        expect(gcd(-2, -3)).toEqual(1);
     });
     it("examples of one being multiple of other", () => {
         expect(gcd(3, 9)).toEqual(3);
@@ -28,12 +28,12 @@ describe("GreatestCommonDivisorTest", () => {
     it("common factor examples", () => {
         expect(gcd(6, 8)).toEqual(2);
         expect(gcd(49, 315)).toEqual(7);
-        expect(gcd(-24, -28)).toEqual(-4);
+        expect(gcd(-24, -28)).toEqual(4);
     });
 
     it("negative examples", () => {
         expect(gcd(-24, 28)).toEqual(4);
-        expect(gcd(24, -28)).toEqual(-4);
+        expect(gcd(24, -28)).toEqual(4);
 
 
     });
