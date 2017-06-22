acbcdffc8bb33ca5e41e1376efe45fa405c5d555
DIWithers <d.ivy.withers@gmail.com>
2017-06-22 07:53:19 -0400

    equals works, except for reducing

--- spec/FractionEquals.spec.ts
+++ spec/FractionEquals.spec.ts
@@ -3,4 +3,16 @@ describe("FractionsEqualsTest", () => {
     it("fractions with same numerator and denominator are equal ", () => {
         expect(new Fraction(3, 5)).toEqual(new Fraction(3, 5));
     });
+    it("fractions with different numerators are not equal ", () => {
+        expect(new Fraction(1, 5)).not.toEqual(new Fraction(2, 5));
+    });
+    it("fractions with different denominators are not equal ", () => {
+        expect(new Fraction(3, 4)).not.toEqual(new Fraction(3, 7));
+    });
+    it("whole numbers equal corresponding fraction ", () => {
+        expect(new Fraction(5, 1)).toEqual(new Fraction(5));
+    });
+    it("whole numbers not equal to different whole number ", () => {
+        expect(new Fraction(6)).not.toEqual(new Fraction(5));
+    });
 });
