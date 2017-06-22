c7278a9933bd46b2ee12459ad50736641eeb19f8
DIWithers <d.ivy.withers@gmail.com>
2017-06-22 08:31:39 -0400

    alternate: inline for readability

--- spec/AddFractions.spec.ts
+++ spec/AddFractions.spec.ts
@@ -1,18 +1,15 @@
 import {Fraction} from "../src/AddFractions";
 
 describe("AddFractionsTest", () => {
-    function checkAddFractionsAsIntegers(addend: number, augend: number, expectedResult: number): void {
-        expect(new Fraction(addend).plus(new Fraction(augend))).toEqual(new Fraction(expectedResult));
-    }
 
     it("zero plus zero", () => {
-        checkAddFractionsAsIntegers(0, 0, 0);
+        expect(new Fraction(0).plus(new Fraction(0))).toEqual(new Fraction(0));
     });
     it("non-zero plus zero", () => {
-        checkAddFractionsAsIntegers(3, 0, 3);
+        expect(new Fraction(3).plus(new Fraction(0))).toEqual(new Fraction(3));
     });
     it("zero plus non-zero", () => {
-        checkAddFractionsAsIntegers(0, 5, 5);
+        expect(new Fraction(0).plus(new Fraction(5))).toEqual(new Fraction(5));
     });
     it("non-negative, non-zero operands ", () => {
         expect(new Fraction(3).plus(new Fraction(4))).toEqual(new Fraction(7));
@@ -21,7 +18,6 @@ describe("AddFractionsTest", () => {
         expect(new Fraction(-3).plus(new Fraction(1))).toEqual(new Fraction(-2));
     });
     it("non-trivial denominator of the same value", () => {
-        let sum: Fraction = new Fraction(1, 5).plus(new Fraction(2, 5));
-        expect(new Fraction(3, 5)).toEqual(sum);
+        expect(new Fraction(3, 5)).toEqual(new Fraction(1, 5).plus(new Fraction(2, 5)));
     });
 });
