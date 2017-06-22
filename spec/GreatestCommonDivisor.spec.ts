0199d24eb19b8a7820f5a3339efc9611f60fa9e7
DIWithers <d.ivy.withers@gmail.com>
2017-06-22 12:53:49 -0400

    explored GCD algorithm, but alternating negative num and denom gives undesired behavior

--- spec/GreatestCommonDivisor.spec.ts
+++ spec/GreatestCommonDivisor.spec.ts
@@ -11,7 +11,30 @@ describe("GreatestCommonDivisorTest", () => {
         return a;
     }
 
-    it("one and one", () => {
+    it("reflexive examples", () => {
         expect(gcd(1, 1)).toEqual(1);
+        expect(gcd(2, 2)).toEqual(2);
+        expect(gcd(-1, -1)).toEqual(-1);
+    });
+    it("relatively prime examples", () => {
+        expect(gcd(2, 3)).toEqual(1);
+        expect(gcd(4, 7)).toEqual(1);
+        expect(gcd(-2, -3)).toEqual(-1);
+    });
+    it("examples of one being multiple of other", () => {
+        expect(gcd(3, 9)).toEqual(3);
+        expect(gcd(5, 30)).toEqual(5);
+    });
+    it("common factor examples", () => {
+        expect(gcd(6, 8)).toEqual(2);
+        expect(gcd(49, 315)).toEqual(7);
+        expect(gcd(-24, -28)).toEqual(-4);
+    });
+
+    it("negative examples", () => {
+        expect(gcd(-24, 28)).toEqual(4);
+        expect(gcd(24, -28)).toEqual(-4);
+
+
     });
 });
