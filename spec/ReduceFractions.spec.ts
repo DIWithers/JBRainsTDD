5460fc90dfeaa640d3268853942bb8a23e22851c
DIWithers <d.ivy.withers@gmail.com>
2017-06-23 16:00:42 -0400

    reduce a fraction in the contructor(incrementel change)

--- spec/ReduceFractions.spec.ts
+++ spec/ReduceFractions.spec.ts
@@ -5,7 +5,7 @@ describe("ReduceFractionsTest", () => {
     it("fraction already in lowest terms", () => {
         expect(new Fraction(3, 4)).toEqual(new Fraction(3, 4));
     });
-    // it("reduce to non-whole numbers", () => {
-    //     expect(new Fraction(6, 8)).toEqual(new Fraction(3, 4  ));
-    // });
+    it("reduce to non-whole numbers", () => {
+        expect(new Fraction(6, 8)).toEqual(new Fraction(3, 4  ));
+    });
 });
